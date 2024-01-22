cc78eeda516fff91c09c86fd3b018ff09fcb2c5d
Emery Weis <emeryweis@gmail.com>
2024-01-22 12:47:39 -0600

    prints host name

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 import java.net.InetAddress;
 public class Hello{
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        String name = InetAddress.getLocalHost().getHostName();
+        try {
+            //Scanner in = new Scanner(System.in);
+            String name = InetAddress.getLocalHost().getHostName();
 
-        //System.out.println("What is your name: ");
-        //name = in.nextLine();
-        System.out.println("Hello " + name + "!");
+            //System.out.println("What is your name: ");
+            //name = in.nextLine();
+            System.out.println("Hello " + name + "!");
+        }
+        catch (Exception E) {
+            System.err.println(E.getMessage());
+        }
     }
 }
