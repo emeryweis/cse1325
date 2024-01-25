5e0f71e77a03848fa9fdcfacb4a42f087c04fc59
Emery Weis <emeryweis@gmail.com>
2024-01-25 11:25:16 -0600

    I REALLY hope this is right

--- P02/full_credit/Note.java
+++ P02/full_credit/Note.java
@@ -1,5 +1,25 @@
 public class Note{
-    public void Note{
-
+    
+    //default constructor
+    public Note() {
+        pitch = null;
     }
+    //constructor w/ parameters
+    public Note(Pitch pitch, int octave) {
+        this.pitch = pitch;
+        if (octave < -5) octave = -5;
+        if (octave > 4) octave = 4;
+    }
+    //toString method including override
+    @Override
+    public String toString() {
+        if(pitch == null) return " ";
+        else return (pitch.toString() + octave);
+        
+    }
+    
+    //private fields
+    private Pitch pitch;
+    private int octave;
+
 }
