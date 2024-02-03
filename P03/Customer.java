290c76419be3c57c9cea18ab01fc24248f724d2e
Emery Weis <emeryweis@gmail.com>
2024-02-03 09:43:10 -0600

    added random number generation

--- P03/Customer.java
+++ P03/Customer.java
@@ -1,6 +1,18 @@
 public class Customer {
 
     public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+        int astIndex;
+
+        if (email.indexOf("@") == -1) {
+            throw new IllegalArgumentException("Invalid email address: " + email);
+        } else {
+            astIndex = email.indexOf("@");
+        }
+        if (email.indexOf(".", astIndex) == -   1 ) {
+            throw new IllegalArgumentException("Invalid email address: " + email);
+        }
         
     }
 
--- P03/Product.java
+++ P03/Product.java
@@ -1,17 +1,20 @@
 public class Product {
 
-   public Product product(String name, int price) {
+   public Product (String name, int price) {
       if (price < 0) {
         throw new IllegalArgumentException("Inavlid price of " + name + ": " + price);
       }
 
+      Product.nextStockNumber = (int)Math.floor(Math.random() * (1000 - 0 + 1) + 0);
+
       this.name = name;
       this.price = price;
-
    }
+
    public int getStockNumber(){
 
    }
+
    public int getPrice(){
 
    }
@@ -21,7 +24,7 @@ public class Product {
 
    }
    
-    private static int nextStockNumber();
+    private static int nextStockNumber;
     private int stockNumber;
     private String name;
     private int price;
