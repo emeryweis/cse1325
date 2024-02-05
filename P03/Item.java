dfa7a919f40b536f4b9fcb4fcc7c97fa24abf904
Emery Weis <emeryweis@gmail.com>
2024-02-05 12:34:10 -0600

    forgot to add Item and build.xml

---
+++ P03/Item.java
@@ -0,0 +1,19 @@
+public class Item {
+
+    public Item(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    } 
+    
+    int getPrice() {
+        return quantity * product.price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%3d %-40s $ %-40d", quantity, product, getPrice());
+    }
+
+    private Product product;
+    private int quantity;
+}
