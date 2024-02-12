eacf368859ada2bd1a89463983db1bdaac03d13f
Emery Weis <emeryweis@gmail.com>
2024-02-12 12:47:48 -0600

    IDK how this inheritance is working

---
+++ P04/store/Plant.java
@@ -0,0 +1,14 @@
+package store;
+
+public class Plant extends Product{
+    private Exposure exposure;
+
+    public Plant(String species, int price, Exposure exposure) {
+        super("Plant: " + species, price);
+        this.exposure = exposure;
+    }
+
+    public Exposure getExposure(){
+        return exposure;
+    }
+}
