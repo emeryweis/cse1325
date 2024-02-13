bc46dcb52f9cde5df00e880ac95a4dab56e63eed
Emery Weis <emeryweis@gmail.com>
2024-02-12 21:59:32 -0600

    test package

---
+++ P04/test/TestProduct.java
@@ -0,0 +1,54 @@
+import store.Product;
+
+public class TestProduct {
+    public static void main(String[] args) {
+        int vector = 1;
+        int result = 0;
+    
+        // TEST VECTOR: Throws IllegalArgumentException on negative price
+        try {
+            store.Product p = new store.Product("Bad", -1);
+            System.err.println("FAIL: Product accepted negative price");
+            result |= vector;
+        } catch(IllegalArgumentException e) {
+        } catch(Exception e) {
+            System.err.println("FAIL: Product threw wrong exception on negative price:\n" + e);
+            result |= vector;
+        }
+        vector <<= 1;
+        
+        // TEST VECTOR: Creates correct stock numbers
+        store.Product p1 = new store.Product("Valiant Apricot Vinca", 1195);
+        store.Product p2 = new store.Product("Valiant Lilac Vinca", 1295);
+        if(p1.getStockNumber() != 0 || p2.getStockNumber() != 1) {
+            System.err.println("FAIL: Incorrect Product stock number "
+                             + p1.getStockNumber() + " (0 expected) or "
+                             + p2.getStockNumber() + " (1 expected)");
+            result |= vector;
+        }
+        vector <<= 1;
+        
+        // TEST VECTOR: Returns correct prices
+        if(p1.getPrice() != 1195 || p2.getPrice() != 1295) {
+            System.err.println("FAIL: Incorrect Product price "
+                             + p1.getPrice() + " (1195 expected) or "
+                             + p2.getPrice() + " (1295 expected)");
+            result |= vector;
+        }
+        vector <<= 1;
+        
+        // TEST VECTOR: Creates correct String representation
+        String p1Expected = "Valiant Apricot Vinca          $   11.95";
+        if(!p1.toString().equals(p1Expected)) {
+            System.err.println("FAIL: Incorrect Product toString:");
+            System.err.println("  returned '" + p1 + "'");
+            System.err.println("  expected '" + p1Expected + "'");
+            result |= vector;
+        }
+
+        if(result != 0) {
+            System.err.println("\nFAILED with error code " + result);
+            System.exit(result);
+        }
+    }
+}
