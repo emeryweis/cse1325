f2a98a202c4de3b78af97c9380edb050dedd9c70
Emery Weis <emeryweis@gmail.com>
2024-02-13 07:43:01 -0600

    added javadocs

--- P04/store/Order.java
+++ P04/store/Order.java
@@ -1,9 +1,25 @@
+/**
+ * Creates a custom order.
+ * 
+ * @author              Emery David Weis
+ * @version             0.2
+ * @since               0.1
+ * 
+ * 
+ */
+
+
+
 package store;
 
 import java.util.ArrayList;
 
 public class Order {
-
+    /**
+     * 
+     * @param customer      A customer from Customer.java
+     * @since               0.1        
+     */
     public Order(Customer customer) {
         this.customer = customer;
         orderNumber = nextOrderNumber;
@@ -11,15 +27,24 @@ public class Order {
 
 
     }
-
+    /**
+     * 
+     * @param item          An item from Item.java
+     * @since               0.1
+     */
     public void addItem(Item item) {
         items.add(item);
     }
-
+    /**
+     * @returns int         Total order price as an int.
+     */
     public int getPrice() {
         return getPrice();
     }
-
+    /**
+     * @returns String      A formatted order description
+     * @since               0.1
+     */
     @Override 
     public String toString() {
         return String.format("Order " + orderNumber + " for " + customer + "\n" 
