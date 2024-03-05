d7fd76c66cb0213b0fe33bc569ddcc595fe5251a
Emery Weis <emeryweis@gmail.com>
2024-03-04 18:07:38 -0600

    working on writing and reading

--- P06/store/Item.java
+++ P06/store/Item.java
@@ -1,11 +1,24 @@
 package store;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class Item {
 
     public Item(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
     } 
+
+    public Item(BufferedReader br) throws IOException {
+        this.quantity = Integer.parseInt (br.readLine());
+    }
+
+    public void save(BufferedWriter bw) throws IOException {
+        bw.write(product + "\n");
+        bw.write(quantity + "\n");
+    }
     
     int getPrice() {
         return quantity * product.getPrice();
