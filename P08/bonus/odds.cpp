87873941068b68b936bb82cb4e28607055609a00
Emery Weis <emeryweis@gmail.com>
2024-03-27 12:56:28 -0500

    working even and odds

--- P08/bonus/odds.cpp
+++ P08/bonus/odds.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;
+
 
 int main(int argc, char* argv[]) {
-    cout << "Enter a sentence";
+    std::cout << "Enter a sentence: ";
 
     std::vector<std::string> odds;
     std::vector<std::string> evens;
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i] % 2) == 0) evens.push_back(argv[i]);
-        if (argv[i] % 2 == 1) odds.push_back(argv[i]);
+        if (std::string(argv[i]).length() % 2 == 0) evens.push_back(argv[i]);
+        
+        if (std::string(argv[i]).length() % 2 == 1) odds.push_back(argv[i]);
     }
 
+    std::cout << "Evens: " << std::endl;
+    for (std::string s : evens) {
+        std::cout << s << " " << std::endl;
+    }
 
-
+    std::cout << "\n" << std::endl;
+    std::cout << "Odds: " << std::endl;
+    for (std::string s : odds) {
+        std::cout << s << " " << std::endl;
+    }
 }
