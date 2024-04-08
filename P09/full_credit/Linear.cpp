8ae8be090c1ab13fa38a737a9e8ad5ebef4115a8
Emery Weis <emeryweis@gmail.com>
2024-04-08 16:41:06 -0500

    added destructors to Polynomial and Linear

--- P09/full_credit/Linear.cpp
+++ P09/full_credit/Linear.cpp
@@ -12,6 +12,9 @@ Linear::Linear(std::vector<double>& coefficients) : Polynomial(coefficients) {}
 std::vector<double> Linear::solve() {
     return std::vector<double> { -_coefficients[1] / _coefficients[0] };
 }
+Linear::~Linear() {
+    delete &_coefficients;
+}
 
 
 
--- P09/full_credit/Polynomial.cpp
+++ P09/full_credit/Polynomial.cpp
@@ -4,3 +4,7 @@
 
 Polynomial::Polynomial(std::vector<double> coefficients) : _coefficients{coefficients} {}
 
+Polynomial::~Polynomial() {
+    delete &_coefficients;
+}
+
