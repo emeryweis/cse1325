50d895a639bd0c097d2942cf973bd7298d72552f
Emery Weis <emeryweis@gmail.com>
2024-04-10 17:27:43 -0500

    trying to understand instructions

--- P10/inch.cpp
+++ P10/inch.cpp
@@ -3,5 +3,15 @@
 #include <iostream>
 #include "inch.h"
 
-Inch::Inch(int whole, int numerator, int denominator) : _whole{whole}, _numerator{numerator}, _denominator{denominator} { }
+Inch::Inch(int whole, int numerator, int denominator) : _whole{whole}, _numerator{numerator}, _denominator{denominator} {
+    validate();
+ }
 Inch::Inch() : Inch(0, 0, 2) { }
+
+Inch Inch::operator+(const Inch& rhs) {
+
+    int new_numerator = (_numerator + rhs._numerator);
+    int new_whole = (_whole + rhs._whole);
+
+    return Inch(new_whole, new_numerator, 64)
+}
--- P10/inch.h
+++ P10/inch.h
@@ -4,19 +4,33 @@
 #define __INCH_H
 
 #include <iostream>
+#include <ostream>
 
 class Inch {
   public:
     Inch(int whole, int numerator, int denominator);
+    Inch();
+
     Inch operator+(const Inch& rhs);
     bool operator==(const Inch& rhs);
     bool operator!=(const Inch& rhs);
+    bool operator<(const Inch& rhs);
+    bool operator<=(const Inch& rhs);
+    bool operator>(const Inch& rhs);
+    bool operator>=(const Inch& rhs);
     
+    friend std::ostream& operator<<(std::ostream& ost, const Inch& inch);
+    friend std::istream& operator>>(std::istream& ist, Inch& inch);
+
+
 
   private:
     int _whole;
     int _numerator;
-    int _denominator
+    int _denominator;
+
+    const int compare(const Inch& rhs);
+    int validate();
 
 };
 
